c456b293a81730efebd8800304d6e6266e4f1db9
Elire Raci <58210135+elire-r@users.noreply.github.com>
2020-06-10 19:59:27 +0200

    Te dhenat per orar

---
+++ stage3admin.java
@@ -0,0 +1,47 @@
+package Application;
+import java.sql.*;
+import java.util.List;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class stage3admin {
+
+	private static Stage mainstage;
+	private static Stage mainStage;
+	private static Stage primaryStage = new Stage();
+	// Text fields
+	private static TextField idField = new TextField();
+	private static TextField subjectField = new TextField();
+	private static TextField ectsField = new TextField();
+	private static TextField profField = new TextField();
+	private static TextField assField = new TextField();
+	private static TextField LditaField = new TextField();
+	private static TextField LoraField = new TextField();
+	private static TextField LsallaField = new TextField();
+	private static TextField UditaField = new TextField();
+	private static TextField UoraField = new TextField();
+	private static TextField UsallaField = new TextField();
+	private static TextField nstudField = new TextField();
+	private static TextField departamentiField = new TextField();
+	private static TextField semField = new TextField();
+	private static TextField grupiField = new TextField();
+	
+	
+	
+	
+
+}
