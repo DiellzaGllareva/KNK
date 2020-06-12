c01b6a319bc21a68ebf252ad9dd7ca5d987d6d0d
AritaAlidemaj <58113020+AritaAlidemaj@users.noreply.github.com>
2020-06-12 21:16:23 +0200

    shtimi i databazes

---
+++ databaza/databaza.sql
@@ -0,0 +1,16 @@
+create database projektiKNK;
+use projektiKNK;
+
+INSERT INTO `orari` (`id`, `lenda`, `ects`, `profesori`, `asistenti`, `ldita`, `lora`, `lsalla`, `udita`, `usalla`, `uora`, `niveli`, `departamenti`, `semestri`, `grupi`) 
+VALUES (NULL, 'Sinjale dhe Sisteme', '5', 'Vjosa Shatri', 'Drilon Bunjaku', 'E hene', '08:00-10:30', '626', 'E marte', '310', '13:00-14:30', 'Bachelor', 'FIEK', 'III', '1'),
+ (NULL, 'Elektronika', '5', 'Myzafere Limani', 'Qamil Kabashi', 'E enjte', '11:00-12:30', '626', 'E premte', '626', '08:00-09:30', 'Bachelor', 'FIEK', 'III', '1'), 
+ (NULL, 'Matematika 3K', '5', 'Qefsere Doko Gjonbalaj', 'Blerta Selimaj', 'E premte', '10:30-12:00', '411', 'E marte', '415', '16:00-17:30', 'Bachelor', 'FIEK', 'III', '1'),
+ (NULL, 'Baza e Te Dhenave', '5', 'Lule Ahmedi', 'Besmir Sejdiu', 'E hene', '10:30-12:00', '621', 'E merkure', '621', '17:30-19:00', 'Bachelor', 'FIEK', 'III', '1'),
+ (NULL, 'Interneti', '5', 'Lule Ahmedi', 'Korab Rrmoku', 'E marte', '08:00-09:30', '626', 'E enjte', '611', '15:00-16:30', 'Bachelor', 'FIEK', 'III', '1'), 
+ (NULL, 'Programimi i Orientuar ne Objekte', '5', 'Isak Shabani', 'Vigan Abdurrahmani', 'E merkure', '11:30-13:00', '626', 'E enjte', '611', '08:00-09:30', 'Bachelor', 'FIEK', 'III', '1'),
+ (NULL, 'Komunikimi Njeri Kompjuter', '5', 'Isak Shabani', 'Vigan Abdurrahmani', 'E hene', '13:30-15:00', '621', 'E marte', '611', '18:30-20:00', 'Bachelor', 'FIEK', 'IV', '2'),
+ (NULL, 'Rrjeta Kompjuterike', '5', 'Blerim Rexha', 'Haxhi Lajqi', 'E marte', '08:15-09:45', '621', 'E enjte', '626', '16:00-17:30', 'Bachelor', 'FIEK', 'IV', '2'), 
+ (NULL, 'Siguria e Te Dhenave', '5', 'Blerim Rexha', 'Edon Gashi', 'E merkure', '08:15-09:45', '621', 'E merkure', '615', '11:30-13:00', 'Bachelor', 'FIEK', 'IV', '2'),
+ (NULL, 'Arkitektura e Kompjutereve', '5', 'Valon Raca', 'Vlera Alimehaj', 'E enjte', '13:30-15:00', '621', 'E premte', '621', '13:30-15:00', 'Bachelor', 'FIEK', 'IV','2' ),
+ (NULL, 'Programimi ne Internet,' , '5','Valon Raca','Dardan Shabani','E merkure','13:30-15:00','621','E enjte','621','09:30-11:00','Bachelor','FIEK','IV','2')
+ ;
