d081180aba036f53575d88a878c22e33c2046195
alinafazliu <56960871+alinafazliu@users.noreply.github.com>
2020-06-30 15:02:09 +0200

    Login

--- src/knk/Login.java
+++ src/knk/Login.java
@@ -28,4 +28,47 @@ public class Login extends Application {
 
 	private Connection dbConnection;
 	private TextField IDtxt = new TextField();
+	private PasswordField passwordTxt = new PasswordField();
+	public Stage mainStage;
+	private Button logInButton = new Button("Kyçu");
+	private Label resetpw = new Label("Keni harruar fjalëkalimin?");
+	private Label shqip = new Label("Shqip");
+	private Label english = new Label("English");
+	private Label help = new Label("Ndihma");
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		mainStage = primaryStage;
+		dbConnection = DBConnection.getConnection();
+		// setConnection();
+
+		BorderPane bpane = new BorderPane();
+		bpane.setPadding(new Insets(70, 200, 70, 200));
+		bpane.setStyle("-fx-background-color:#efefef;");
+		bpane.setPrefSize(850, 500);
+
+		GridPane gridPane = new GridPane();
+		gridPane.setHgap(10);
+		gridPane.setVgap(10);
+		gridPane.setPadding(new Insets(10, 10, 10, 10));
+		gridPane.setStyle("-fx-background-color:#f2f2f2;");
+		gridPane.setEffect(new DropShadow(8, Color.BLACK));
+		gridPane.setAlignment(Pos.CENTER);
+		gridPane.setMinWidth(395);
+		gridPane.setMinHeight(450);
+
+//LOGO NE LOGIN PAGE
+		Image image = new Image("/images/unipr.png");
+		ImageView logo = new ImageView(image);
+		logo.setSmooth(true);
+		gridPane.add(logo, 1, 0);
+
+//RRESHTI PERDORUESI
+		Label ID = new Label("Përdoruesi:");
+		ID.setStyle("-fx-font-size:15;");
+		IDtxt.setPromptText("ID");
+		IDtxt.setStyle("-fx-font-size:15;");
+		IDtxt.setMaxWidth(150);
+		gridPane.add(ID, 0, 1);
+		gridPane.add(IDtxt, 1, 1);
 	
